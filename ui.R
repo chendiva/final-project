7bb1e9344ce7e7d61baf7a8fde4189eb71204c3f
Chen Yu-Ting <chendiva1245@gmail.com>
2016-11-30 17:23:38 -0800

    three pages setting

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ shinyUI(navbarPage('Procedures across USA in 2014',
                    # Create a tab panel for your charts
                    tabPanel('State level analyzation',
                             titlePanel('Analyztion of different kind of procedures in state level'),
-                            # Create sidebar layout for the in-depth analyzation from different levels.
+                            # Create sidebar layout for the in-depth analyzation from state level.
                             sidebarLayout(
                               
                               # Side panel for controls
@@ -39,7 +39,7 @@ shinyUI(navbarPage('Procedures across USA in 2014',
                    
                    tabPanel('Hopital level analyzation',
                             titlePanel('Analyztion of various procedures in the hospital level'),
-                            # Create sidebar layout for the in-depth analyzation from different levels.
+                            # Create sidebar layout for the in-depth analyzation from hospital level.
                             sidebarLayout(
                               
                               # Side panel for controls
@@ -47,8 +47,7 @@ shinyUI(navbarPage('Procedures across USA in 2014',
                                 
                                 # Input to select variable to chats
                                 #In the hospital level, there are two ways to compare, the bubble chart offers you a way to compare certain surgery among all the hospitals that offered their statistics.
-                                #The pie chart based on the hospital level offer you a choice of hospital you want to look into, and show the proportion of the procedures that the hospital had conducted.
-                      
+                                
                                 selectInput('ycol', 'Type of Surgery for hospital level',
                                             choices = list("Gastrointestinal surgery" = 'Gastrointestinal', "Eye surgery" = 'Eye',
                                                            "Nervous System surgery"= 'Nervous.System', "Musculoskeletal surgery"= 'Musculoskeletal',
@@ -57,7 +56,29 @@ shinyUI(navbarPage('Procedures across USA in 2014',
                                                            "Other kind of surgery" = 'Other'
                                             ),
                                             selected = "Gastrointestinal"
-                                ),
+                                )
+                              ),  
+                              
+                              # Main panel: display plotly charts and a table that you can search for a certain hospital's information about surgery.
+                              mainPanel(
+                            
+                                plotlyOutput('bubble')
+                              )
+                            )
+                            
+                   ),
+                   tabPanel('Search a hospital',
+                            titlePanel('Search specific hospital data'),
+                            # Create sidebar layout for the hopital that the user want to look at.
+                            sidebarLayout(
+                              
+                              # Side panel for controls
+                              sidebarPanel(
+                                
+                               # Input to select variable to chats
+                               #The pie chart based on the hospital level offer you a choice of hospital you want to look into, and show the proportion of the procedures that the hospital had conducted.
+                               #A table contains the hospital name, state, and procedures' numbers 
+                                
                                 selectInput('ycol3', 'Hopital choice',
                                             choices = df2$Hospital.Name ,              
                                             
@@ -67,8 +88,8 @@ shinyUI(navbarPage('Procedures across USA in 2014',
                               # Main panel: display plotly charts and a table that you can search for a certain hospital's information about surgery.
                               mainPanel(
                                 
-                                plotlyOutput('bubble'),DT::dataTableOutput('table1'),
-                                plotlyOutput('pie.hospital')
+                                plotlyOutput('pie.hospital'), DT::dataTableOutput('table1')
+                                
                               )
                             )
                             
